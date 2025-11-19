54a81a88b5e46955ba12567065a89b9f24e16522
mutantsan <kvaqich@gmail.com>
2025-11-19 14:45:22 +0200

    feat: update pdkit to weasyprint

--- ckanext/metaexport/formatters/pdf_format.py
+++ ckanext/metaexport/formatters/pdf_format.py
@@ -1,5 +1,4 @@
-import pdfkit
-from six import StringIO
+from weasyprint import HTML
 
 from . import Format
 
@@ -10,8 +9,7 @@ class PDFFormat(Format):
     def __init__(self):
         super().__init__()
 
-    def render(self, template, *args, **kwargs):
+    def render(self, template, *args, **kwargs) -> bytes:
         html = super().render(template, *args, **kwargs)
 
-        result = pdfkit.from_file(StringIO(html), False)
-        return result
+        return  HTML(string=html).write_pdf() or b""
